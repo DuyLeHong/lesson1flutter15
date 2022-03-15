238bf9614536333000d270200f7d3652cf4d28eb
Duy Le <lehongduy.bk@gmail.com>
2022-03-15 20:15:59 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'CodeFresher - Flutter 15'),
+      home: const MyHomePage(title: 'CF - Flutter 15 - Bai 1'),
     );
   }
   
